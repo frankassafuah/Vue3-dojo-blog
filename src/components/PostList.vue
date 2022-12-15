620af8684d2f9ab144f9dd9cf4290c7fe6bed2d8
Frank Assafuah <frankassafuah@gmail.com>
2022-12-15 13:50:00 +0000

    Lifecycle hooks

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -5,15 +5,30 @@
 </template>
 
 <script>
+import { onMounted, onUnmounted, onUpdated } from "@vue/runtime-core";
 import SinglePost from "./SinglePost.vue";
 export default {
+  name: "PostList",
+  components: { SinglePost },
   props: {
     posts: {
       type: Array,
       default: () => [],
     },
   },
-  components: { SinglePost },
+
+  setup() {
+    //lifecycle hooks
+    onMounted(() => {
+      console.log("mounted");
+    });
+    onUnmounted(() => {
+      console.log("Unmounted");
+    });
+    onUpdated(() => {
+      console.log("Updated");
+    });
+  },
 };
 </script>
 
